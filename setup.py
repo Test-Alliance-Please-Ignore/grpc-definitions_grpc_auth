06a6934cb9428560e3b52a09a89076f7d882bb67
ozzeh <ozzeh@pleaseignore.com>
2018-11-13 06:44:48 -0600

    v0.5, added EsiService, minor whitespace/linting cleanup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='grpc_testauth',
-    version='0.4',
+    version='0.5',
     packages=['grpc_testauth'],
     install_requires = [
         'grpcio',
         'googleapis-common-protos'
     ],
-    zip_safe = False
+    zip_safe=False
 )
