8b78fbd44f7c685cf9e54aa968fca0bd2687460e
Tross Yvormes <trossyvormes@protonmail.com>
2019-10-23 23:24:32 -0400

    0.10

--- grpc_testauth/main_pb2_grpc.py
+++ grpc_testauth/main_pb2_grpc.py
@@ -3,6 +3,7 @@ import grpc
 
 from grpc_testauth import esi_pb2 as grpc__testauth_dot_esi__pb2
 from grpc_testauth import group_pb2 as grpc__testauth_dot_group__pb2
+from grpc_testauth import ping_pb2 as grpc__testauth_dot_ping__pb2
 from grpc_testauth import user_pb2 as grpc__testauth_dot_user__pb2
 
 
@@ -163,4 +164,46 @@ def add_EsiServiceServicer_to_server(servicer, server):
   }
   generic_handler = grpc.method_handlers_generic_handler(
       'grpc_testauth.EsiService', rpc_method_handlers)
+  server.add_generic_rpc_handlers((generic_handler,))
+
+
+class PingServiceStub(object):
+  # missing associated documentation comment in .proto file
+  pass
+
+  def __init__(self, channel):
+    """Constructor.
+
+    Args:
+      channel: A grpc.Channel.
+    """
+    self.Search = channel.unary_unary(
+        '/grpc_testauth.PingService/Search',
+        request_serializer=grpc__testauth_dot_ping__pb2.PingSearchRequest.SerializeToString,
+        response_deserializer=grpc__testauth_dot_ping__pb2.PingSearchResponse.FromString,
+        )
+
+
+class PingServiceServicer(object):
+  # missing associated documentation comment in .proto file
+  pass
+
+  def Search(self, request, context):
+    # missing associated documentation comment in .proto file
+    pass
+    context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+    context.set_details('Method not implemented!')
+    raise NotImplementedError('Method not implemented!')
+
+
+def add_PingServiceServicer_to_server(servicer, server):
+  rpc_method_handlers = {
+      'Search': grpc.unary_unary_rpc_method_handler(
+          servicer.Search,
+          request_deserializer=grpc__testauth_dot_ping__pb2.PingSearchRequest.FromString,
+          response_serializer=grpc__testauth_dot_ping__pb2.PingSearchResponse.SerializeToString,
+      ),
+  }
+  generic_handler = grpc.method_handlers_generic_handler(
+      'grpc_testauth.PingService', rpc_method_handlers)
   server.add_generic_rpc_handlers((generic_handler,))
